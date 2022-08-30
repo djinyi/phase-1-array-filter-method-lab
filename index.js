04607c106a1e6ee9080709991b8f9ff8d578b9f0
Daisy Yi <daisyyi@rocketmail.com>
2022-08-29 18:36:15 -0700

    commit tree 6a71a3aaab2b3ebeac411ed7c52d213b931ccf35

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+const drivers = 
+  ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(collection) {
+
+    const newDrivers = []
+
+    for (const user of collection) {
+        if (user.toLowerCase() === "sammy") {
+            console.log(user);
+        }
+        if (user.toLowerCase() === "bobby") {
+          console.log(user);
+        }
+        else (newDrivers.push(user))
+    }
+}
+
+findMatching(drivers);
